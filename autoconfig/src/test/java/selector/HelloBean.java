f45847687e560799ee54497d80459def30a2c238
ats3059 <ats3059@naver.com>
2023-06-05 21:07:07 +0900

    스프링 자동설정 스터디

---
+++ autoconfig/src/test/java/selector/HelloBean.java
@@ -0,0 +1,4 @@
+package selector;
+
+public class HelloBean {
+}
---
+++ autoconfig/src/test/java/selector/HelloConfig.java
@@ -0,0 +1,16 @@
+package selector;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfig {
+
+    @Bean
+    public HelloBean helloBean(){
+        return new HelloBean();
+    }
+
+
+
+}
